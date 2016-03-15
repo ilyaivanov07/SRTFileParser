57f0d7282a7f1bbdbdfa11350589b3c3dcf43ead
Ilya <ilyaivanov07@users.noreply.github.com>
2016-03-15 14:40:13 -0400

    Create CircularArrayTraverser.py

---
+++ CircularArrayTraverser.py
@@ -0,0 +1,38 @@
+# 1 2 3 4
+# 5 6 7 8
+# 9 10 11 12
+# 13 14 15 16
+twoDimensionalArray = [[1, 2, 3, 4],[5, 6, 7, 8],[9, 10, 11, 12],[13, 14, 15, 16]]
+
+colIndex = 0
+rowIndex = 0
+matrixSize = 4
+rowIncrement = 0
+colIncrement = 1
+
+for inner in twoDimensionalArray:
+    for val in inner:
+        print twoDimensionalArray[rowIndex][colIndex]
+        # go down
+        if ((rowIndex + colIndex) == (matrixSize - 1)) and (rowIndex < colIndex):
+            colIncrement = 0
+            rowIncrement = 1
+            rowIndex += 1
+        # go left
+        elif rowIndex == colIndex and rowIndex >= matrixSize/2:
+            colIndex -= 1
+            colIncrement = -1
+            rowIncrement = 0
+        # go up
+        elif (rowIndex + colIndex) == (matrixSize - 1) and rowIndex > colIndex:
+            colIncrement = 0
+            rowIncrement = -1
+            rowIndex -= 1
+        # go right
+        elif (rowIndex - colIndex) == 1 and rowIndex < matrixSize/2:
+            colIndex += 1
+            colIncrement = 1
+            rowIncrement = 0
+        else:
+            colIndex += colIncrement
+            rowIndex += rowIncrement
