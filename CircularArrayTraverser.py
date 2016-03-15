89e2120de105834ca5194378e8fa7beb5e4afb05
Ilya <ilyaivanov07@users.noreply.github.com>
2016-03-15 18:14:21 -0400

    Update CircularArrayTraverser.py

--- CircularArrayTraverser.py
+++ CircularArrayTraverser.py
@@ -1,7 +1,4 @@
-# 1 2 3 4
-# 5 6 7 8
-# 9 10 11 12
-# 13 14 15 16
+
 twoDimensionalArray = [[1, 2, 3, 4],[5, 6, 7, 8],[9, 10, 11, 12],[13, 14, 15, 16]]
 
 colIndex = 0
@@ -9,10 +6,11 @@ rowIndex = 0
 matrixSize = 4
 rowIncrement = 0
 colIncrement = 1
+result = []
 
 for inner in twoDimensionalArray:
     for val in inner:
-        print twoDimensionalArray[rowIndex][colIndex]
+        result.append(twoDimensionalArray[rowIndex][colIndex])
         # go down
         if ((rowIndex + colIndex) == (matrixSize - 1)) and (rowIndex < colIndex):
             colIncrement = 0
@@ -36,3 +34,4 @@ for inner in twoDimensionalArray:
         else:
             colIndex += colIncrement
             rowIndex += rowIncrement
+print result
